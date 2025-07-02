abdfcedd56dd00dc6395341802876f67541b4169
MANISHKUMAR <velpulamanishpatel@gmail.com>
2025-07-02 12:37:33 +0530

    Add files via upload

---
+++ working Files/Traning.js
@@ -0,0 +1,16 @@
+document.write("Hello Django Traning");
+alert("Hello");
+var wifi = "Testing";
+document.write(wifi);
+
+var a = prompt("Enter your name : ","java Traning");
+
+document.write(a);
+var a = prompt ("Enter your Roll: ");
+if ((a == 10))
+    document.write("Name: Manish");
+var b = prompt("ENter your Age: ");
+if (( b >= 18))
+    document.write("Elegible : Rudra");
+else
+document.write("Not Elegible")
